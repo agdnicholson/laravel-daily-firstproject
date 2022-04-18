25c802b9c6f81da1d4c412f1b492345acfc93557
Andrew Nicholson <agdnicholson@gmail.com>
2022-04-18 13:09:24 +0100

    Posts table and displaying posts on home page and making the category links work.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,13 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Post;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $allCategories = Category::all();
+        $categories = Category::all();
+        $posts = Post::when(request(key: 'category_id'), function ($query) {
+                $query->where('category_id', request(key: 'category_id'));
+            })
+            ->latest()
+            ->get();
 
-        return view('index', ['categories' => $allCategories]);
+        return view('index', [
+            'categories' => $categories,
+            'posts' => $posts
+        ]);
     }
 }
